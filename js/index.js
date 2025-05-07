9865c749aea344ef3beafd676036a084284a2f5d
hotchisme <ksue9117@naver.com>
2025-05-07 09:37:01 +0900

    포트폴리오

---
+++ js/index.js
@@ -0,0 +1,131 @@
+$(function () {
+  /*visual*/
+  $(".visual_wrap").addClass("on");
+  Splitting();
+
+  /*con2*/
+  $("#con2 ul > li > div").on("click", function () {
+    let i = $(this).parent().index();
+
+    $("#imge img").removeClass("active");
+    $("#imge img").eq(i).addClass("active");
+
+    $("#modalBox").addClass("on");
+  });
+
+  $("#xmark").on("click", function () {
+    $("#modalBox").removeClass("on");
+  });
+
+  /*con3*/
+  let total = $(".con3_wrap > ul > li").length;
+  console.log(total);
+
+  let j = 0;
+  start();
+
+  function start() {
+    stop = setInterval(function () {
+      if (j == total - 1) {
+        j = 0;
+      } else {
+        j++;
+      }
+      fade();
+    }, 3000);
+  }
+
+  $(".con3_wrap .right").on("click", function () {
+    clearInterval(stop);
+    if (j == total - 1) {
+      j = 0;
+    } else {
+      j++;
+    }
+    fade();
+    start();
+  });
+
+  $(".con3_wrap .left").on("click", function () {
+    clearInterval(stop);
+    if (j == total - 1) {
+      j = 0;
+    } else {
+      j--;
+    }
+    fade();
+    start();
+  });
+
+  $(".con3_wrap .stop").on("click", function () {
+    clearInterval(stop);
+    $(".con3_wrap .play").show()
+    $(".con3_wrap .stop").hide()
+
+  });
+
+  $(".con3_wrap .play").on("click", function () {
+    $(".con3_wrap .stop").show()
+    $(".con3_wrap .play").hide()
+
+    start();
+  });
+
+  function fade() {
+    $(".con3_wrap > ul > li").fadeOut();
+    $(".con3_wrap > ul > li").eq(j).fadeIn();
+  }
+
+  let visual = $("#visual").offset().top;
+  let con1 = $("#con1").offset().top;
+  let con2 = $("#con2").offset().top;
+  let con3 = $("#con3").offset().top;
+  let con4 = $("#con4").offset().top;
+
+  console.log(visual, con1, con2, con3, con4);
+
+  $(window).on("scroll", function () {
+    let sc = $(this).scrollTop();
+    console.log(sc);
+
+    if (sc >= 50) {
+      $("#header").addClass("in");
+    } else {
+      $("#header").removeClass("in");
+    }
+
+    if (sc >= visual && sc < con1) {
+      $(".con1_left ").addClass("on");
+      $(".con1_right ").addClass("on");
+      $(".con1_lp").addClass("in");
+  
+    }
+
+    if (sc >= con1 && sc < con2) {
+      $("#con2").addClass("active");
+
+    }
+    if (sc >= con2 && sc < con3) {
+
+    }
+
+    if (sc >= con3 && sc < con4) {
+      $(".con3_wrap").addClass("on");
+
+    }
+
+    if (sc >= con4) {
+
+    }
+
+    $("#header li").on("click", function () {
+      let i = $(this).index();
+      // console.log(i);
+      let target = $("#wrap > div").eq(i).offset().top;
+      console.log(target);
+      $("html").stop().animate({ scrollTop: target });
+    });
+  });
+
+  
+});
